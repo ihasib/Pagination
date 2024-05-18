294479615c2866cb2d8845f401afbec5c6a02b59
Hasib <hasib.jhd@gmail.com>
2024-05-18 21:46:37 +0600

    https request applied

--- Pagination/ViewController.swift
+++ Pagination/ViewController.swift
@@ -12,8 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fetchUsers()
     }
 
+    func fetchUsers() {
+        print("fetch users called")
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        let urlRequest = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            guard let data = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
 
 }
 
